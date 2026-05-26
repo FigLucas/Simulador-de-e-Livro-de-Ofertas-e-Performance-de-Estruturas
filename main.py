d60a832d48587829b483e1ab13133eed78e01224
anaosello <anacsosello@gmail.com>
2026-05-26 16:39:45 -0300

    fix: estrutura principal do projeto

---
+++ main.py
@@ -0,0 +1,5 @@
+#############################################################
+# Simulador de Livros de Oferta e Performance de  Estruturas
+
+# Grupo 2: Ana Carolina Saggioratto Osello, 
+#############################################################
